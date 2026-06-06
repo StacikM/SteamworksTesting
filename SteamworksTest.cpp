16de410b966248dd52717579086fdff2dfd69b82
StacikM <146170881+StacikM@users.noreply.github.com>
2026-06-06 19:19:20 +0200

    Lobby & rich presense stuff, with styling

--- SteamworksTest.cpp
+++ SteamworksTest.cpp
@@ -5,11 +5,74 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <unordered_set>
+#include <limits>
 #include <steam_api.h>
 
 std::string nameButGlobal;
 CSteamID steamIdButGlobalToo;
 uint64 steamId64ButGlobalToo;
+
+class LobbyPrinter // i hate steam
+{
+public:
+    void PrintLobbies();
+
+private:
+    void OnLobbyMatchList(LobbyMatchList_t* pResult, bool bIOFailure);
+
+    CCallResult<LobbyPrinter, LobbyMatchList_t> m_CallResult;
+};
+
+void LobbyPrinter::PrintLobbies()
+{
+    SteamAPICall_t handle =
+        SteamMatchmaking()->RequestLobbyList();
+
+    m_CallResult.Set(handle, this,
+        &LobbyPrinter::OnLobbyMatchList);
+}
+void LobbyPrinter::OnLobbyMatchList(LobbyMatchList_t* pResult, bool bIOFailure)
+{
+    if (bIOFailure || !pResult)
+        return;
+
+    for (int i = 0; i < pResult->m_nLobbiesMatching; i++)
+    {
+        CSteamID lobby =
+            SteamMatchmaking()->GetLobbyByIndex(i);
+
+        printf("%llu\n", lobby.ConvertToUint64());
+    }
+}
+
+class LobbyManager
+{
+public:
+    CCallResult<LobbyManager, LobbyCreated_t> m_CreateCall;
+
+    void Create();
+private:
+    void OnLobbyCreated(LobbyCreated_t* pResult, bool bIOFailure);
+};
+void LobbyManager::Create()
+{
+    SteamAPICall_t call =
+        SteamMatchmaking()->CreateLobby(k_ELobbyTypePublic, 8);
+
+    m_CreateCall.Set(call, this, &LobbyManager::OnLobbyCreated);
+}
+void LobbyManager::OnLobbyCreated(LobbyCreated_t* pResult, bool bIOFailure)
+{
+    if (bIOFailure || pResult->m_eResult != k_EResultOK)
+        return;
+
+    CSteamID lobbyID = pResult->m_ulSteamIDLobby;
+
+    printf("Created lobby: %llu\n", lobbyID.ConvertToUint64());
+
+    SteamMatchmaking()->SetLobbyData(lobbyID, "name", "Test Lobby");
+}
 
 void SetAppID(std::string what)
 {
@@ -34,8 +97,17 @@ std::string GetAppID()
 
 void TheChoice() {
     std::string thechoice;
-    std::cout << "\n\nType a number:\n\n1 - Your Player Info\n\n2 - Game Info\n\n3 - Change App ID (achievements, etc)\n\n4 - Your Friends\n\n5 - Achievement List\n\n6 - Unlock Achievement\n\n7 - Remove Achievement\n\n8 - Exit\n\nPick Your Choice: ";
-    std::cin >> thechoice;
+    std::cout <<
+        "\n\nType a number:\n\n"
+        "1 - Your Player Info        2 - Game Info\n"
+        "3 - Change App ID           4 - Your Friends\n"
+        "5 - Achievement List        6 - Unlock Achievement\n"
+        "7 - Remove Achievement      8 - Current App ID info\n"
+        "9 - List lobbies           10 - Create a test lobby (deleted on exit)\n"
+        "11 - Set Rich Presence\n\n"
+        "exit - Exit\n\n"
+        "Pick Your Choice: ";    std::cin >> thechoice;
+    LobbyManager lobbymanager;
     if (thechoice == "1") {
         std::cout << "\n\n---- Player Info ----" << std::endl;
         std::cout << "Name: ";
@@ -152,8 +224,87 @@ void TheChoice() {
         SteamUserStats()->StoreStats();
         std::cout << "\nRemoved! if the game dev has set that only the backend can remove the achievement, nothing will happen.";
     }
-    else if (thechoice == "8") {
+    else if (thechoice == "exit") {
         std::exit(0);
+    }
+    else if (thechoice == "8") {
+        int buildid = SteamApps()->GetAppBuildId();
+        char installPath[1024];
+
+        uint32 len = SteamApps()->GetAppInstallDir(
+            SteamUtils()->GetAppID(),
+            installPath,
+            sizeof(installPath)
+        );
+        std::string languages = SteamApps()->GetAvailableGameLanguages();
+        std::string currentLanguage = SteamApps()->GetCurrentGameLanguage();
+
+        std::cout << "Game Info for ";
+        std::cout << GetAppID();
+        std::cout << "\n------------\n"; 
+        std::cout << "App Build ID: ";
+        std::cout << buildid;
+        std::cout << "\nInstall path: " << installPath;
+        std::cout << "\nLanguages: ";
+        std::cout << languages;
+        std::cout << "\nCurrent language: ";
+        std::cout << currentLanguage;
+
+    }
+    else if (thechoice == "9") {
+        LobbyPrinter lobbyPrinter;
+        lobbyPrinter.PrintLobbies();
+    }
+    else if (thechoice == "10") {
+        lobbymanager.Create();
+    }
+    else if (thechoice == "11") {
+        std::unordered_set<std::string> validKeys = {
+            "state", "location", "mode", "details"
+        };
+
+        auto getKey = [&](const std::string& prompt) -> std::string {
+            std::string key;
+            while (true)
+            {
+                std::cout << prompt;
+                std::getline(std::cin, key);
+
+                if (validKeys.count(key))
+                    return key;
+
+                std::cout << "Invalid key. Use: state, location, mode, details\n";
+            }
+            };
+
+        auto getValue = [&](const std::string& prompt) -> std::string {
+            std::string value;
+            std::cout << prompt;
+            std::getline(std::cin, value);
+            return value;
+            };
+
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+        std::string key1 = getKey("Key1 (state, location, mode, details): ");
+        std::string value1 = getValue("Value1: ");
+
+        std::string key2 = getKey("Key2 (state, location, mode, details): ");
+        std::string value2 = getValue("Value2: ");
+
+        std::string key3 = getKey("Key3 (state, location, mode, details): ");
+        std::string value3 = getValue("Value3: ");
+
+        auto set = [](const std::string& k, const std::string& v) {
+            if (!k.empty() && !v.empty())
+                SteamFriends()->SetRichPresence(k.c_str(), v.c_str());
+            };
+
+        set(key1, value1);
+        set(key2, value2);
+        set(key3, value3);
+
+        std::cout << "\nDone! Check your rich presence:\nhttps://steamcommunity.com/dev/testrichpresence\n";
     }
     else {
         std::cout << "\nEnter a valid number";
@@ -196,5 +347,6 @@ int main()
     std::cout << GetAppID() << std::endl;
     while (true) {
         TheChoice();
+        SteamAPI_RunCallbacks();
     }
 }
